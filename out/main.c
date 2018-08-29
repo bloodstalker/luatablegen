2dce41448fc1f139fd433c9f95c4e21d82ac445b
bloodstalker <thabogre@gmail.com>
2018-08-29 12:10:41 +0430

    update

---
+++ out/main.c
@@ -0,0 +1,10 @@
+#include "./wasm_tables.h"
+#include <stdio.h>
+#include <inttypes.h>
+#include "/home/bloodstalker/devi/hell2/bruiser/lua-5.3.4/src/lauxlib.h"
+#include "/home/bloodstalker/devi/hell2/bruiser/lua-5.3.4/src/lua.h"
+#include "/home/bloodstalker/devi/hell2/bruiser/lua-5.3.4/src/lualib.h"
+
+#pragma weak main
+int main (int argc, char** argv) {
+}
